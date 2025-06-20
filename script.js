ebe97e46fe49f54d9f5a45fca4e41015bbe40efc
Pedro Camargo <pedro01cesarsanto@gmail.com>
2025-06-20 18:37:00 -0300

    Atualizando a pagina web toda

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector('.cta-button');
-  button.addEventListener('mouseenter', () => {
-    button.style.boxShadow = '0 0 15px #ff4b2b';
-  });
-  button.addEventListener('mouseleave', () => {
-    button.style.boxShadow = 'none';
-  });
-});
